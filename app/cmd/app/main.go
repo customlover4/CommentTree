73d07335458d1ddfb4a54614dd3bf5032b208218
Daniil Ulyanov <smdfuck@icloud.com>
2025-09-11 22:04:19 +0300

    test main, carcas for web layer, template example, update configs(yml, docker)

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,3 +1,59 @@
 package main
 
-func main() {}
+import (
+	"CommentTree/internal/entities/comment"
+	"CommentTree/internal/service"
+	"CommentTree/internal/storage"
+	"CommentTree/internal/storage/postgres"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wb-go/wbf/config"
+	"github.com/wb-go/wbf/zlog"
+)
+
+var (
+	ConnString = "postgresql://dev:qqq@localhost:5432/test?sslmode=disable"
+	ConfigPath = "../config/config.yml"
+)
+
+func init() {
+	zlog.Init()
+
+	tmp := os.Getenv("POSTGRES_CONN")
+	if tmp != "" {
+		ConnString = tmp
+	}
+
+	tmp = os.Getenv("CONFIG_PATH")
+	if tmp != "" {
+		ConfigPath = tmp
+	}
+}
+
+func main() {
+	cfg := config.New()
+	err := cfg.Load(ConfigPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if cfg.GetString("debug") == "false" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	p := postgres.New(ConnString)
+	str := storage.New(p)
+	// p.Create(comment.Comment{Message: "привет бро"})
+	// p.Create(comment.Comment{Message: "здарова", ParentID: 4})
+	// p.Create(comment.Comment{Message: "здарова опять", ParentID: 4})
+	fmt.Println(p.Comments(0, &comment.GetterOpts{
+		Substr: "здарова",
+	}))
+	srv := service.New(str)
+
+	_ = srv
+
+}
---
+++ app/internal/web/router.go
@@ -0,0 +1,9 @@
+package web
+
+import (
+	"CommentTree/internal/service"
+
+	"github.com/wb-go/wbf/ginext"
+)
+
+func Routes(router *ginext.Engine, s *service.Service) {}
