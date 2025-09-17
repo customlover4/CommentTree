9c5e749163beb4e43ea646177684015c1863e40d
Daniil Ulyanov <smdfuck@icloud.com>
2025-09-17 21:56:19 +0300

    feat: add foreign key error

--- app/pkg/errs/errs.go
+++ app/pkg/errs/errs.go
@@ -3,7 +3,11 @@ package errs
 import "errors"
 
 var (
-	// Database Errors
+	// From db realisation to upper layer use ErrDB preffix.
+	// ErrDBNotAffected on update/delete when no one fields not affected.
 	ErrDBNotAffected = errors.New("no one rows didn't affected")
+	// ErrDBNotFound on get when we can't find our field.
 	ErrDBNotFound = errors.New("not found")
+	// ErrDBViolatesForeignKey when foreign key is wrong.
+	ErrDBViolatesForeignKey = errors.New("violates foreign key")
 )
