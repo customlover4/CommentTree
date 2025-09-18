56b33645cf7eb9494afa976ad1206d72dddaa51c
Daniil Ulyanov <smdfuck@icloud.com>
2025-09-18 09:39:38 +0300

    feat: update and add new entities to entities/comment.go

--- app/internal/entities/comment/comment.go
+++ app/internal/entities/comment/comment.go
@@ -6,7 +6,20 @@ type Comment struct {
 	ParentID int64
 }
 
+type CommentView struct {
+	ParentComment Comment
+	Childs        []Comment
+}
+
 type GetterOpts struct {
 	Page   int    `form:"page"`
 	Substr string `form:"substr"`
+}
+
+func (g *GetterOpts) Empty() bool {
+	if g.Page == 0 && g.Substr == "" {
+		return true
+	}
+
+	return false
 }
